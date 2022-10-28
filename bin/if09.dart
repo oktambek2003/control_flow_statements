6d19d2fea0b4529d48129ed1d7023ae9b7043c41
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-28 15:56:50 +0500

    9-misol

--- bin/if09.dart
+++ bin/if09.dart
@@ -9,5 +9,15 @@
         a: integer
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False. */
+bool func(int a) {
+  int b = a % 10;
+  int d = a ~/ 10;
+  int c = b * 10 + d;
+  bool ans;
+  ans = a == c;
+  return ans;
+}
 
-void main() {}
+void main() {
+  print(func(23));
+}
