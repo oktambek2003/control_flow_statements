99ae49f904bc564fd14a2afd4cb83e61dcd295f5
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-28 15:30:08 +0500

    7-misol

--- bin/if07.dart
+++ bin/if07.dart
@@ -11,4 +11,25 @@
         a: integer
     Returns:
         string: the message to print */
-void main() {}
+String func(int a) {
+  String b='the number is zero';
+  if (a > 0) {
+    if (a % 2 == 1) {
+      b = 'positive odd number';
+    } else {
+      b = 'positive even number';
+    }
+  }
+  if (a < 0) {
+    if (a % 2 == 1) {
+      b = 'negative odd number';
+    } else {
+      b = 'negative even number';
+    }
+  }
+  return b;
+}
+
+void main() {
+  print(func(0));
+}
